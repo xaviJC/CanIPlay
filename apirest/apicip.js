3a819ee4b8b3544b6ff2b4566ec8a1a901d23913
pmarsa <patmar30@icloud.com>
2021-01-07 09:50:31 +0100

    api get buscador

--- apirest/apicip.js
+++ apirest/apicip.js
@@ -31,71 +31,108 @@ app.get('/', (req,res) => {
 // ---------------------------------- JUEGOS ----------------------------------
 // ---------------------------------- GET ---------------------------------Patig
 
- app.get("/juegos", (req, res) => {
-   let game_name = req.query.name
-   let game_pegi = req.query.pegi
-   let game_platform = req.query.platform
+//  app.get("/juegos", (req, res) => {
+//    let game_name = req.query.name
+//    let game_pegi = req.query.pegi
+//    let game_platform = req.query.platform
 
-    if (game_name != null) {
-      let params = [game_name]
-      let paramsComas = `%${params}%`;
-      myQuery = `SELECT *
-                FROM juegos
-                JOIN juegos_plataforma ON juegos.id_juego = juegos_plataforma.id_juego
-                WHERE titulo_juego LIKE ?`
-        connection.query(myQuery,paramsComas, function(err, result) {
-          if (err) {
-            respuesta = "Ha ocurrido un error: " + err+ "."
-          } else if (result.length === 0) {
-            respuesta = "No se ha encontrado ningún resultado: " + err+ "."
-          } else {
-            respuesta = result
-          }
-          res.send(respuesta)
-        })
+//     if (game_name != null) {
+//       let params = [game_name]
+//       let paramsComas = `%${params}%`;
+//       myQuery = `SELECT *
+//                 FROM juegos
+//                 JOIN juegos_plataforma ON juegos.id_juego = juegos_plataforma.id_juego
+//                 WHERE titulo_juego LIKE ?`
+//         connection.query(myQuery,paramsComas, function(err, result) {
+//           if (err) {
+//             respuesta = "Ha ocurrido un error: " + err+ "."
+//           } else if (result.length === 0) {
+//             respuesta = "No se ha encontrado ningún resultado: " + err+ "."
+//           } else {
+//             respuesta = result
+//           }
+//           res.send(respuesta)
+//         })
+//     }
+
+//     if (game_pegi) {
+//       if (req.query.pegi) {
+//         let params = [game_pegi]
+//         myQuery = `SELECT *
+//                    FROM juegos
+//                    JOIN juegos_plataforma ON juegos.id_juego = juegos_plataforma.id_juego
+//                    WHERE pegi = ?`
+//         connection.query(myQuery, params, function(err, result) {
+//           if (err) {
+//             respuesta = "Ha ocurrido un error: " + err + "."
+//           } else if (result.length === 0) {
+//             respuesta = "No se ha encontrado ningún resultado: " + err + "."
+//           } else {
+//             respuesta = result
+//           }
+//           res.send(respuesta)
+//         })
+//       }
+//     }
+
+    // if (game_platform) {
+    //   if (req.query.platform) {
+    //     let plataforma = [game_platform]
+    //     console.log(plataforma)
+    //     myQuery = `SELECT * FROM juegos RIGHT JOIN juegos_plataforma ON juegos.id_juego = juegos_plataforma.id_juego WHERE ${plataforma}`
+    //   connection.query(myQuery, function(err, result) {
+    //     if (err) {
+    //       respuesta = "Ha ocurrido un error: " + err + "."
+    //     } else if (result.length === 0) {
+    //       respuesta = "No se ha encontrado ningún resultado: " + err + "."
+    //     } else {
+    //       respuesta = result
+    //     }
+    //     res.send(respuesta)
+    //   })
+    //   }
+    // }
+//  });
+
+  app.get("/juegos", (req, res) => {
+    let game_name = req.query.name
+    let game_pegi = req.query.pegi
+    let game_platform = req.query.platform
+    let params  = new Array();
+    // let and = new Array();
+    let query = `SELECT * FROM juegos JOIN juegos_plataforma ON juegos.id_juego = juegos_plataforma.id_juego WHERE `
+    console.log(req.query.platform)
+
+    if (game_name) {
+      params.push(`%${game_name}%`);
+      // and.push(` titulo_juego LIKE ? AND`)
+      query += ` titulo_juego LIKE ? AND`
     }
-
     if (game_pegi) {
-      if (req.query.pegi) {
-        let params = [game_pegi]
-        myQuery = `SELECT *
-                   FROM juegos
-                   JOIN juegos_plataforma ON juegos.id_juego = juegos_plataforma.id_juego
-                   WHERE pegi = ?`
-        connection.query(myQuery, params, function(err, result) {
-          if (err) {
-            respuesta = "Ha ocurrido un error: " + err + "."
-          } else if (result.length === 0) {
-            respuesta = "No se ha encontrado ningún resultado: " + err + "."
-          } else {
-            respuesta = result
-          }
-          res.send(respuesta)
-        })
-      }
+      params.push(game_pegi);
+      query += ` pegi = ? AND`
+    }
+    if (game_platform) {
+      params.push(game_platform);
+      query += ` ${game_platform} AND`
     }
 
-    if (game_platform) {
-      if (req.query.platform) {
-        let plataforma = [game_platform]
-        console.log(plataforma)
-        myQuery = `SELECT * FROM juegos RIGHT JOIN juegos_plataforma ON juegos.id_juego = juegos_plataforma.id_juego WHERE ${plataforma}`
-      connection.query(myQuery, function(err, result) {
-        if (err) {
-          respuesta = "Ha ocurrido un error: " + err + "."
-        } else if (result.length === 0) {
-          respuesta = "No se ha encontrado ningún resultado: " + err + "."
-        } else {
+    myQuery = query.substring(0,query.length - 3)
+    console.log(myQuery)
+    console.log(params)
+
+    connection.query(myQuery, params, function(err, result) {
+      if (err) {
+        respuesta = "Ha ocurrido un error: " + err + "."
+      } else if (result.length === 0) {
+        respuesta = "No se ha encontrado ningún resultado: " + err + "."
+      } else {
           respuesta = result
-        }
+      }
         res.send(respuesta)
       })
-      }
-    }
- });
 
-
-
+  })
 
 
 
